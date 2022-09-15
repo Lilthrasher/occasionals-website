e43a029981b3787235e6114977d860f2c1ec2376
Lilthrasher <ghostlysmith@gmail.com>
2022-09-15 16:37:24 -0700

    Changed javascript

--- js/scripts.js
+++ js/scripts.js
@@ -33,11 +33,7 @@ function clipboard2() {
 function clipboard3() {
     // Get the text field
     var copyText = document.getElementById("copy-3");
-  
-    // Select the text field
-    copyText.select();
-    copyText.setSelectionRange(0, 99999); // For mobile devices
-  
+
      // Copy the text inside the text field
     navigator.clipboard.writeText(copyText.value);
 }
